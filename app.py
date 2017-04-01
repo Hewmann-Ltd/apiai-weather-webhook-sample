54419a473148bca8f9356b517ae9d48f404516df
Mark Purdon <mark@hewmann.com>
2017-04-01 21:18:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Today in " + location.get('city') + " airport"  + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + "degrees farenheit"
 
     print("Response:")
     print(speech)
@@ -95,7 +95,7 @@ def makeWebhookResult(data):
         "displayText": speech,
         # "data": data,
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        "source": "Oblige Global Limited"
     }
 
 
